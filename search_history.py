57792fdf9cd91319e1bb9aa97cacb6b942c4aa30
Alex Leake <mail@alexleake.co.uk>
2016-01-20 07:29:23 +0000

    added some comments.

--- search_history.py
+++ search_history.py
@@ -10,16 +10,17 @@ from ricloud.api import RiCloud
 example_credentials = "APPLE_ID='foo@bar.com'\nAPPLE_PASSWORD=1234"
 
 # check for the credentials file
-
 if os.path.isfile('./credentials.py'):
     from credentials import APPLE_ID, APPLE_PASSWORD
 else:
     print "credentials.py file missing, please create one. ie:\n{cred}".format(cred=example_credentials)
     raise SystemExit
 
+# if all exists, try and connect to the api
 api = RiCloud()
 api.login(APPLE_ID, APPLE_PASSWORD)
 
+# then request history for each device
 for device in api.devices:
     data = api.backup_client.request_data(device_id=device, data_mask=4)
     print device, data
