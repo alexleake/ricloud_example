37d90977b0d7ec3c18abe64b19a5f52a1bf04248
Alex Leake <mail@alexleake.co.uk>
2016-01-20 07:55:10 +0000

    added a check for test creds, if used it will import the fake json doc.

--- search_history.py
+++ search_history.py
@@ -16,16 +16,17 @@ else:
     print "credentials.py file missing, please create one. ie:\n{cred}".format(cred=example_credentials)
     raise SystemExit
 
-# test if variables are test credentials
 if APPLE_ID == 'foo@bar.com' and APPLE_PASSWORD == 1234:
-    print "these are test credentials"
-    raise SystemExit
+    # test if variables are test credentials
+    print "these are test credentials, search will be performed against test document."
+    from test_data import data 
+else:
+    # if all exists, try and connect to the api
+    api = RiCloud()
+    api.login(APPLE_ID, APPLE_PASSWORD)
 
-# if all exists, try and connect to the api
-api = RiCloud()
-api.login(APPLE_ID, APPLE_PASSWORD)
+    # then request history 
+    for device in api.devices:
+        data = api.backup_client.request_data(device_id=device, data_mask=4)
 
-# then request history for each device
-for device in api.devices:
-    data = api.backup_client.request_data(device_id=device, data_mask=4)
-    print device, data
+print data
