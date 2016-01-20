8c3e74219cf1242cd5bcb556c6345dc0b7219ca3
Alex Leake <mail@alexleake.co.uk>
2016-01-20 07:27:38 +0000

    added a small check for the "credentials.py" file.

--- search_history.py
+++ search_history.py
@@ -5,7 +5,17 @@ import json
 import os
 
 from ricloud.api import RiCloud
-from credentials import APPLE_ID, APPLE_PASSWORD
+
+# example of credentials file
+example_credentials = "APPLE_ID='foo@bar.com'\nAPPLE_PASSWORD=1234"
+
+# check for the credentials file
+
+if os.path.isfile('./credentials.py'):
+    from credentials import APPLE_ID, APPLE_PASSWORD
+else:
+    print "credentials.py file missing, please create one. ie:\n{cred}".format(cred=example_credentials)
+    raise SystemExit
 
 api = RiCloud()
 api.login(APPLE_ID, APPLE_PASSWORD)
